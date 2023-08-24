5f8a392e0d4cf6fa8ac37c9bfd5318bbe8ad67ca
edidiesky <essienedidiong1000@gmail.com>
2023-08-24 08:44:19 -0700

    text

--- client/src/components/home/product_list/index.tsx
+++ client/src/components/home/product_list/index.tsx
@@ -7,15 +7,22 @@ interface ProductCardProps {
 const product = [
     {
         background: "hsla(206.81564245810057, 89.95%, 60.98%, 1.00)",
+        subimage1: "https://assets.website-files.com/5d85edd208e53eed3ae194a2/5e2861c21b5059478eca7d83_blaubeere-1.png",
+        subimage2: "https://assets.website-files.com/5d85edd208e53eed3ae194a2/5e2861bf19edde6d808fc650_blaubeere-2.png",
         image: "https://assets.website-files.com/5d85edd208e53eed3ae194a2/5e2c2f7fd2e06746e4a50f05_blueberry.png",
+
 
     },
     {
         background: "hsla(14.361702127659573, 100.00%, 63.14%, 1.00)",
+        subimage2: "https://assets.website-files.com/5d85edd208e53eed3ae194a2/5e2861bf19edde6d808fc650_blaubeere-2.png",
+        subimage1: "https://assets.website-files.com/5d85edd208e53eed3ae194a2/5d88d8d6249f213cee30a048_fruits-1.png",
         image: "https://assets.website-files.com/5d85edd208e53eed3ae194a2/5e2c2e658c2b172ab3b7acbd_peach.png",
     },
     {
         background: "hsla(87.96116504854368, 50.24%, 59.80%, 1.00)",
+        subimage1: "https://assets.website-files.com/5d85edd208e53eed3ae194a2/5d88e2c7a39a4857976f2c3b_lime.svg",
+        subimage2: "https://assets.website-files.com/5d85edd208e53eed3ae194a2/5d88e2c4a711b1675fb5129e_limete.png",
         image: "https://assets.website-files.com/5d85edd208e53eed3ae194a2/5e2c2f94d2e067c112a50fab_lime.png"
     }
 ]
@@ -33,18 +40,20 @@ const ProductList: React.FC = () => {
                             <div className="product_card_wrapper auto flex column gap-2">
                                 <div className="image_wrappers auto">
                                     <img src={x.image} alt="" className="image auto" />
+                                    <img src={x.subimage1} alt="" className="images image_2" />
+                                    <img src={x.subimage2} alt="" className="images image_3" />
+s
                                     <div className="flex card_btn_wrapper w-100 justify-center item-center">
-                                        <div backgroundColor={x.background} className="card_btn fs-20 flex justify-center item-center family1 text-white text-light">
+                                        <ProductCardBtn backgroundColor={x.background} className="fs-20 flex justify-center item-center family1 text-white text-light">
                                             <div className="text">View Product</div>
-                                        </div></div>
+                                        </ProductCardBtn>
+                                    </div>
                                 </div>
 
                             </div>
                         </ProductCard>
                     })
                 }
-                {/* <img src="https://assets.website-files.com/5d820ccf1097dd2dccadc680/5d824200708f0f8c1ba3cd96_fruits-3.png" alt="" className="image image_2" />
-                <img src="https://assets.website-files.com/5d820ccf1097dd2dccadc680/5d8240fdd06ccc780fa240d0_fruits-1.png" alt="" className="image image_3" /> */}
 
             </div>
         </PorductListStyles>
@@ -64,6 +73,24 @@ const ProductCard = styled.div<ProductCardProps>`
     .image_wrappers {
         width: 40%;
         position: relative;
+        .images {
+    position: absolute;
+    width:13rem;
+    height:13rem;
+    z-index: 5;
+    &.image_2 {
+        right: -100%;
+        top: -20%;
+        width:17rem;
+    height:17rem;
+    }
+    &.image_3 {
+        left: -100%;
+        bottom: 0%;
+        width:15rem;
+    height:15rem;
+    }
+  }
         img {
         width: 100%;
         }
@@ -118,6 +145,47 @@ const ProductCard = styled.div<ProductCardProps>`
     
   }
 `;
+
+const ProductCardBtn = styled.div<ProductCardProps>`
+
+        /* padding:1.6rem 3rem; */
+        width:12rem;
+        height: 5rem;
+        border-radius: 40px;
+        background: #000;
+        overflow: hidden;
+        cursor: pointer;
+        position: relative;
+      transition: all 0.7s ease;
+
+        &:is(:hover, :focus-visible) {
+            transform:scale(1.1);
+        }
+        .text {
+      z-index: 10;
+
+        }
+        &:is(:hover, :focus-visible)::before {
+      bottom: -10%;
+      transform:translateX(-50%) scale(1.4);
+      
+    }
+        &::before {
+      content: "";
+      position: absolute;
+      bottom: -200%;
+      left: 50%;
+      transform: translateX(-50%);
+      width: 40%;
+      height: 50%;
+      border-radius: 100%;
+      background-color: ${(props) => props.backgroundColor || "white"};
+  /* Add more styles for the card here */
+      transition: all 1s ease;
+      z-index: 1;
+      padding: 2rem;
+    }
+`
 
 
 const PorductListStyles = styled.div`
